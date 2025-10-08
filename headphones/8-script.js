23b85b6540fcf7fe90f5772e2629959022b14704
Frimpong Michael <frimpstchalla@gmail.com>
2025-10-08 21:36:34 +0100

    added 8-script.js

---
+++ headphones/8-script.js
@@ -0,0 +1,10 @@
+
+const hamburger = document.getElementById('hamburger');
+const navLinks = document.getElementById('nav-links');
+
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('active');
+    navLinks.classList.toggle('open');
+});
+
+console.log('JS file loaded');
